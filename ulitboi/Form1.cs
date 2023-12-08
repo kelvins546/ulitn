7a85742a778603b9e512d48b395b5577bc23327b
kelvin <kken5848@gmail.com>
2023-12-08 09:00:51 +0800

    first commit

---
+++ ulitboi/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ulitboi
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          if(MainClass.IsValidUser(textBox1.Text, textBox2.Text)== false) 
+            {
+                MessageBox.Show("Invalid username or password " , "Information", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+
+
+            }
+            else
+            {
+                this.Hide();
+                frmMain frm = new frmMain();
+                frm.Show();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ulitboi/MainClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ulitboi
+{
+    internal class MainClass
+
+    {
+        public static readonly string con_string = "LAPTOP-38FKTTFI\\SQLEXPRESS; Initial Catalog=RM;Persist Security Info= True; User ID=sa; Password=123";
+        public static SqlConnection con =new SqlConnection(con_string);
+  
+    
+
+
+        public static bool IsValidUser(string user, string pass)
+        {
+            bool isValid = false;
+            string qry= @"Select *from users where username='"+user+"' and upass ='"+pass+"'";
+            SqlCommand cmd = new SqlCommand(qry, con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                isValid = true;
+            }
+
+
+            return isValid;
+        
+        }
+    }
+}
